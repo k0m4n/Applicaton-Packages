c4b77dc0a4eadddef2ff2e7d654a084bc48b2343
k0m4n <k0m4n@k0m4n.online>
2023-01-22 16:47:36 -0800

    Update Author Name

--- Toolkit/Deploy-Application.ps1
+++ Toolkit/Deploy-Application.ps1
@@ -115,7 +115,7 @@ Try {
     [String]$appRevision = '01'
     [String]$appScriptVersion = '1.0.0'
     [String]$appScriptDate = 'XX/XX/20XX'
-    [String]$appScriptAuthor = '<author name>'
+    [String]$appScriptAuthor = 'k0m4n'
     ##*===============================================
     ## Variables: Install Titles (Only set here to override defaults set by the toolkit)
     [String]$installName = ''
